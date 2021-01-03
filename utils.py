d115fb7401229afa4ad93858ce311aa41a0bccac
Sai Sasank <sai.sasank.yadati@gmail.com>
2021-01-03 19:36:59 +0530

    default path

--- utils.py
+++ utils.py
@@ -2,9 +2,7 @@ from matplotlib import animation
 import matplotlib.pyplot as plt
 import numpy as np
 
-def save_frames_as_gif(frames, fps=60, path='./', filename='gym_animation.gif'):
-
-    #Mess with this to change frame size
+def save_frames_as_gif(frames, fps=60, filename='gym_animation.gif'):
     plt.figure(figsize=(frames[0].shape[1] / 72.0, frames[0].shape[0] / 72.0), dpi=72)
 
     patch = plt.imshow(frames[0])
@@ -14,7 +12,7 @@ def save_frames_as_gif(frames, fps=60, path='./', filename='gym_animation.gif'):
         patch.set_data(frames[i])
 
     anim = animation.FuncAnimation(plt.gcf(), animate, frames = len(frames), interval=50)
-    anim.save(path + filename, writer='imagemagick', fps=fps)
+    anim.save('./' + filename, writer='imagemagick', fps=fps)
 
 
 def get_state_rep_func(maze_size):
